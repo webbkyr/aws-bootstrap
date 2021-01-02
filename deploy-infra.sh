c1e6c9838f967f7e83bca6aba87f371942f2cbf9
Kayla Webb <kaylarwebb@gmail.com>
2021-01-01 22:07:13 -0500

    Create infrastructure via Cfn

---
+++ deploy-infra.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+STACK_NAME=awsbootstrap
+REGION=us-east-1
+
+EC2_INSTANCE_TYPE=t2.micro
+
+echo -e "\n\n========= Deploying main.yml ==========="
+
+aws cloudformation deploy \
+  --region $REGION \
+  --stack-name $STACK_NAME \
+  --template-file main.yml \
+  --no-fail-on-empty-changeset \
+  --capabilities CAPABILITY_NAMED_IAM \
+  --parameter-overrides \
+    EC2InstanceType=$EC2_INSTANCE_TYPE
+
+if [ $? -eq 0 ]; then
+  aws cloudformation list-exports \
+      --query "Exports[?Name=='InstanceEndpoint'].Value"
+fi
